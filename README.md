b8fe46593413b5842f5593fe1deb6022bc88720b
Shisato <shisatoyano@gmail.com>
2020-12-24 23:06:08 +0900

    implemented node define and rotation

--- README.md
+++ README.md
@@ -4,3 +4,13 @@
 * Difference between heights of left and right subtrees can not be more than one for all nodes.  
 * Most of BST operations take O(height) time.  
 * If we make sure that height of the tree remains O(Log n) after every insertion and deletion, then we can guarantee an upper bound of O(Log n) for all these operations.  
+
+1. perform the normal Binary Search Tree.  
+2. current node must be one of the ancestors of the newly inserted node.  
+3. update the height of the current node.  
+4. get the balance factor of the current node.  
+5. left subtree height - right subtree height
+6. if balance factor is greater than 1, then the current node is unbalanced and we are either in left left case or left right case.  
+7. to check whether it is left left case or not, compare the newly inserted key with the key in left subtree root.  
+8. if balance factor is less than -1, then the current node is unbalanced and we are either in right right case or right left case.  
+9. to check whether it is right right case or not, compare the newly inserted key with key in right subtree root.
--- main.cpp
+++ main.cpp
@@ -1,4 +1,56 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// tree node
+class Node {
+public:
+    int key;
+    Node* left;
+    Node* right;
+    int height;
+};
+
+// get maximum of two integers
+int max(int a, int b) {
+    return (a > b)? a : b;
+}
+
+// get the height of tree
+int height(Node* N) {
+    if (N == NULL) {
+        return 0;
+    }
+    return N->height;
+}
+
+// allocate a new node with the given key
+// and NULL left and right pointers
+Node* new_node(int key) {
+    Node* node = new Node();
+    node->key = key;
+    node->left = NULL;
+    node->right = NULL;
+    node->height = 1; // new node is initially added at leaf
+
+    return node;
+}
+
+// right rotate subtree rooted with y
+Node* right_rotate(Node* y) {
+    Node* x = y->left;
+    Node* T2 = x->right;
+
+    // perform rotation
+    x->right = y;
+    y->left = T2;
+
+    // update heights
+    y->height = max(height(y->left), height(y->right)) + 1;
+    x->height = max(height(x->left), height(x->right)) + 1;
+
+    // return new root
+    return x;
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
